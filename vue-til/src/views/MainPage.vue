509c699dcf7e42d4bce9f937fc2a9eb8a7deab9f
wangsungtaek <tjdxorv@naver.com>
2022-03-12 12:49:18 +0900

    학습노트 데이터 조회

--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -3,12 +3,46 @@
     <h1>Main</h1>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
+      <LoadingSpinner v-if="isLoading"></LoadingSpinner>
+      <ul v-else>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        ></PostListItem>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { fetchPosts } from '@/api/index.js';
+import PostListItem from '@/components/posts/PostListItem.vue';
+import LoadingSpinner from '@/components/common/LoadingSpinner';
+
+export default {
+  data() {
+    return {
+      postItems: [],
+      isLoading: false,
+    };
+  },
+  components: {
+    PostListItem,
+    LoadingSpinner,
+  },
+  methods: {
+    async fetchData() {
+      this.isLoading = true;
+      const { data } = await fetchPosts();
+      this.isLoading = false;
+      this.postItems = data;
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
 </script>
 
 <style></style>
